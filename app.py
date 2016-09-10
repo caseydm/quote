4d52fc855f70df1813aa154a5be1c71aa13eea32
Casey M <caseym@gmail.com>
2016-09-09 22:08:15 -0400

    security setup

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
+from models import User, Role
 
 # app setup
 app = Flask(__name__)
@@ -7,9 +9,23 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='matt@nobien.net', password='password')
+    db.session.commit()
+
+
 @app.route('/')
+@login_required
 def index():
-    return 'hello world!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
