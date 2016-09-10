10676ae064c956c9e0cecb0c83d9b40b68803f77
Casey M <caseym@gmail.com>
2016-09-09 22:05:02 -0400

    model setup

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # app setup
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 
 @app.route('/')
