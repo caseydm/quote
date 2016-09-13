4ac245cac9e9a348cf4eddf575886b353eb7d9cf
Casey M <caseym@gmail.com>
2016-09-13 06:33:22 -0400

    adding blueprint

--- quote/app.py
+++ quote/app.py
@@ -3,8 +3,8 @@ from flask_mail import Mail
 from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
 from quote.models import User, Role, db
 from quote.config import ProdConfig
+from quote import public
 
-# extensions
 mail = Mail()
 security = Security()
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -14,18 +14,25 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 def create_app(config_object=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config_object)
+    register_extensions(app)
+    register_blueprints(app)
+    return app
+
+
+def register_extensions(app):
+    """Register Flask extensions."""
     db.init_app(app)
     mail.init_app(app)
     security.init_app(app, user_datastore)
+    return None
 
-    return app
+
+def register_blueprints(app):
+    """Register Flask blueprints."""
+    app.register_blueprint(public.views.blueprint)
+    return None
 
 app = create_app()
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 
 # Create a user to test with
