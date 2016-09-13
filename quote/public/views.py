b81b9885625108e2432dd17e82f88ae0d44fdbcd
Casey M <caseym@gmail.com>
2016-09-13 06:37:02 -0400

    finished public blueprint

---
+++ quote/public/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Public section, including homepage."""
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('public', __name__, static_folder='../static')
+
+
+@blueprint.route('/')
+def home():
+    render_template('public/index.html')
