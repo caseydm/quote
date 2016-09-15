e5eceba2f0a8d5b9216bb748beb0ca75c66841b2
Casey M <caseym@gmail.com>
2016-09-14 21:55:23 -0400

    working add_role function

--- quote/app.py
+++ quote/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask.ext.security import SQLAlchemyUserDatastore
+from flask.ext.security import SQLAlchemyUserDatastore, user_registered
 from quote.config import ProdConfig
 from quote import public, dashboard
 from quote.extensions import db, mail, security
 from quote.security.models import User, Role
+
 
 # security setup
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -23,6 +24,14 @@ def register_extensions(app):
     mail.init_app(app)
     security.init_app(app, user_datastore)
     db.init_app(app)
+
+    @user_registered.connect_via(app)
+    def when_user_registered(sender, user, confirm_token):
+        """Receive flask-security signal when user registers"""
+        role = user_datastore.find_or_create_role('subscriber')
+        user_datastore.add_role_to_user(user, role)
+        return None
+
     return None
 
 
@@ -32,16 +41,18 @@ def register_blueprints(app):
     app.register_blueprint(dashboard.views.blueprint)
     return None
 
+
 app = create_app()
 
 
 @app.before_first_request
-def create_user():
+def db_setup():
     """Create initial user"""
     db.drop_all()
     db.create_all()
     user_datastore.create_user(email='caseym@gmail.com', password='password')
     db.session.commit()
+
 
 if __name__ == '__main__':
     app.run()
