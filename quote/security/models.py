746240e417704c27f7a21bb79f82e3fa6e4571a2
Casey M <caseym@gmail.com>
2016-09-28 16:03:49 -0500

    added doc strings to security models

--- quote/security/models.py
+++ quote/security/models.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
+"""User models"""
 from flask.ext.security import UserMixin, RoleMixin
 from quote.extensions import db
 
-# models
+
 roles_users = db.Table('roles_users',
     db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
     db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 
 class Role(db.Model, RoleMixin):
+    """User roles"""
+
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
 
 class User(db.Model, UserMixin):
+    """A user for the app"""
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
