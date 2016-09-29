c45d5c4b67d3775f56fa7a3447cbcf40d0bf8fd7
Casey M <caseym@gmail.com>
2016-09-28 22:26:45 -0500

    still working on categories

--- quote/dashboard/views.py
+++ quote/dashboard/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 """Dashboard views"""
+from collections import defaultdict
 from flask import Blueprint, render_template
 from flask.ext.security import login_required
 from .models import Category
 
 blueprint = Blueprint('dashboard', __name__, static_folder='../static')
+
+
+def tree():
+    return defaultdict(tree)
 
 
 @blueprint.route('/dashboard')
@@ -35,4 +40,12 @@ def list_products():
 @login_required
 def edit_categories():
     categories = Category.query.all()
-    return render_template('dashboard/categories.html', categories=categories)
+    roots = [root for root in categories if root.parent_id == 1]
+    items = []
+    for root in roots:
+        items.append({'root': root.name, 'subcategories': []})
+        for category in categories:
+            if category.parent_id == root.id:
+                items[-1]['subcategories'].append(category.name)
+
+    return render_template('dashboard/categories.html', categories=items)
