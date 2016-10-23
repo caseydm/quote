bea922a71130ae4562b613d9513f1a9ae2a14824
Casey M <caseym@gmail.com>
2016-10-23 08:02:12 -0400

    added migrations

--- quote/extensions.py
+++ quote/extensions.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask.ext.security import Security
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 mail = Mail()
 security = Security()
+migrate = Migrate()
