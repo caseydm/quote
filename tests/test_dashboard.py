cb1287ff1fd610e945290daa1c99f5a826a609b1
Casey M <caseym@gmail.com>
2016-10-23 07:34:27 -0400

    added tests for new client

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -9,3 +9,19 @@ class TestDashboardPages:
     def test_dashboard_index(self, client_loggedin):
         response = client_loggedin.get('/dashboard')
         assert b'gary@example.com' in response.data
+
+    def test_dashboard_new_client_page(self, client_loggedin):
+        response = client_loggedin.get('/dashboard/clients/new')
+        assert b'<h2>New Client</h2>' in response.data
+
+class TestDashboardNewClient:
+
+    def test_dashboard_add_client(self, client_loggedin):
+
+        rv = client_loggedin.post('/dashboard/clients/new', data=dict(
+            fname='Joe',
+            lname='Yang',
+            email='joe@example.com'
+            ), follow_redirects=True)
+
+        assert b'Client saved' in rv.data
